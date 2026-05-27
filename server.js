933b948b2f09cd620f101da579a9661beb7fedc8
KwadwoDanso <Kwadwod00@gmail.com>
2026-05-27 02:21:39 -0400

    Add Express server skeleton

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("TaskMaster API");
+// DEPENDENCIES
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// Local Environmental Variables
+const PORT = process.env.PORT || 3001;
+
+// MIDDLEWARE
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.send("TaskMaster API is running...");
+});
+
+// PORT
+app.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}`);
+});
